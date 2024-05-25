6f6ef75b17059577b1e1605188bbbdd709efe314
Filipe Matias <11415688+filipematias23@users.noreply.github.com>
2024-05-25 13:01:13 -0300

    Update Script_EX3.R

--- Script_EX3.R
+++ Script_EX3.R
@@ -22,12 +22,10 @@ library(sf)
 library(stars)
 
 # Uploading one image as example and decreasing the resolution
-EX3<-stack("soybean/11.jpg")
+EX3<-rast("soybean/11.jpg")
+EX3<-imgLAB(EX3)
 # EX3<-aggregate(EX3, fact= 4)
 plotRGB(EX3)
-
-# Shapefile with extent=T (The whole image area will be the shapefile)
-EX.shapeFile<-fieldPolygon(EX3,extent = T)
 
 # Select one index to identify leaves and remove the background
 EX3.I1<- fieldIndex(mosaic = EX3,index = c("SI","BGI","BI"))
@@ -44,11 +42,15 @@ EX3.R<- fieldMask(mosaic = EX3, index = "BGI",
                    cropAbove = T)
 
 # Counting the total number of seeds
-EX.P.Total<-fieldCount(mosaic = EX3.R$mask, 
-                       fieldShape = EX.shapeFile$fieldShape, 
-                       minSize = 0.1,
-                       cex = 1.5,
-                       na.rm = T)
+EX.P.Total<-fieldCount(mosaic = EX3.R$mask,plot = T)
+
+dev.off()
+par(mfrow=c(1,2))
+hist(EX.P.Total$area)
+plotRGB(EX3.R$newMosaic)
+
+Total=EX.P.Total[EX.P.Total$area>15000,]
+dim(Total) #50 seeds
 
 # Select one index to identify green seeds
 EX3.I2<- fieldIndex(mosaic = EX3.R$newMosaic,index = c("SI","BGI","BI"))
@@ -64,16 +66,20 @@ EX3.R2<- fieldMask(mosaic = EX3.R$newMosaic,
                    cropAbove = T)
 
 # Counting the number of green seeds
-EX.Green<-fieldCount(mosaic = EX3.R2$mask, 
-                     fieldShape = EX.shapeFile$fieldShape, 
-                     minSize = 0.07,
-                     cex = 1.5,
-                     na.rm = T)
+EX.Green<-fieldCount(mosaic = EX3.R2$mask,plot = T)
+
+dev.off()
+par(mfrow=c(1,2))
+hist(EX.Green$area)
+plotRGB(EX3.R2$newMosaic)
+
+Green=EX.Green[EX.Green$area>11000,]
+dim(Green) #50 seeds
 
 # Joying information
-data.frame(Total=EX.P.Total$fieldCount,
-           Green=EX.Green$fieldCount,
-           Percentage=round(EX.Green$fieldCount/EX.P.Total$fieldCount,2))
+data.frame(Total=dim(Total)[1],
+           Green=dim(Green)[1],
+           Percentage=round(dim(Green)[1]/dim(Total)[1],3))
 
 ################
 ### Parallel ###
@@ -93,35 +99,30 @@ n.core<-2
 # Starting parallel
 cl <- makeCluster(n.core, output = "")
 registerDoParallel(cl)
-EX.Table.Parallel <- foreach(i = 1:length(pics), .packages = c("raster","FIELDimageR"), 
+EX.Table.Parallel <- foreach(i = 1:length(pics), .packages = c("stars","sf","terra","FIELDimageR"), 
                              .combine = rbind) %dopar% {
-                               EX3<-stack(paste("./soybean/",pics[i],sep = ""))
-                               # EX3<-aggregate(EX3, fact= 4)
-                               EX.shapeFile<-fieldPolygon(EX3,extent = T,
-                                                          plot = F)
-                               EX3.R1<- fieldMask(mosaic = EX3, index = "BGI",
-                                                  cropValue = 0.7,
-                                                  cropAbove = T,
-                                                  plot = F)
-                               EX.P.Total<-fieldCount(mosaic = EX3.R1$mask, 
-                                                      fieldShape = EX.shapeFile$fieldShape, 
-                                                      minSize = 0.1,
-                                                      cex = 1.5,
-                                                      na.rm = T)
-                               EX3.R2<- fieldMask(mosaic = EX3.R1$newMosaic, 
+                               EX3<-rast(paste("./soybean/",pics[i],sep = ""))
+                               EX3<-imgLAB(EX3)
+                               # Removing the background
+                               EX3.R<- fieldMask(mosaic = EX3, index = "BGI",
+                                                 cropValue = 0.7,
+                                                 cropAbove = T)
+                               # Counting the total number of seeds
+                               EX.P.Total<-fieldCount(mosaic = EX3.R$mask,plot = T)
+                               Total=EX.P.Total[EX.P.Total$area>15000,]
+                               # Selecting green seeds
+                               EX3.R2<- fieldMask(mosaic = EX3.R$newMosaic, 
                                                   index = "BI",
                                                   cropValue = 130,
-                                                  cropAbove = T,
-                                                  plot = F)
-                               EX.Green<-fieldCount(mosaic = EX3.R2$mask, 
-                                                    fieldShape = EX.shapeFile$fieldShape, 
-                                                    minSize = 0.07,
-                                                    cex = 1.5,
-                                                    na.rm = T)
-                               data.frame(Total=EX.P.Total$fieldCount,
-                                          Green=EX.Green$fieldCount,
-                                          Percentage=round(EX.Green$fieldCount/EX.P.Total$fieldCount,2))
+                                                  cropAbove = T)
+                               # Counting the number of green seeds
+                               EX.Green<-fieldCount(mosaic = EX3.R2$mask,plot = T)
+                               Green=EX.Green[EX.Green$area>10800,]
+                               data.frame(Total=dim(Total)[1],
+                                          Green=dim(Green)[1],
+                                          Percentage=round(dim(Green)[1]/dim(Total)[1],2))
                              }
+stopCluster(cl)
 rownames(EX.Table.Parallel)<-pics
 EX.Table.Parallel 
 
@@ -131,36 +132,27 @@ EX.Table.Parallel
 
 # Taking individual seed measurements (Remove artifacts by changing the parameter *minArea* and observing the values on EX3.D$Dimension$area)
 dev.off()
-EX3.D<-fieldObject(mosaic = EX3.R$mask, 
-                   watershed = T, 
-                   minArea = 200,
-                   perimeter = F)
 
-# Measurement Output:
-EX3.D$numObjects
-EX3.D$Dimension
+# Taking measurements:
+EX.Obj.D<-fieldCount(mosaic = EX3.R$mask,
+                     plot = T)
 
-# Individual seed visualization:
+EX.Obj.D=EX.Obj.D[EX.Obj.D$area>15000,]
+
+fieldView(EX3,EX.Obj.D,type = 2)
 plotRGB(EX3)
-plot(EX.shapeFile$fieldShape, add=T)
-plot(EX3.D$Objects, add=T, border="red")
-plot(EX3.D$Polygons, add=T, border="blue")
-plot(EX3.D$single.obj[[1]], add=T, col="yellow")
-lines(EX3.D$x.position[[1]], col="red", lty=2)
-lines(EX3.D$y.position[[1]], col="red", lty=2)
-
-# Calculating indices per seed:
-EX3.I<- fieldIndex(mosaic = EX3,index = c("SI","BGI","BI","VARI"))
-EX3.Data<-fieldInfo(mosaic = EX3.I[[c("SI","BGI","BI","VARI")]], fieldShape = EX3.D$Objects, projection = F)
-EX3.Data$fieldShape@data
-
-# Combining the data:
-Data.Obj<-cbind.data.frame(EX3.Data$fieldShape@data,EX3.D$Dimension)
-Data.Obj
+plot(EX.Obj.D$geometry, add=T, border="red")
+plot(EX.Obj.D[1,], add=T, col="yellow")
+EX.Obj.I<- fieldIndex(mosaic = EX3,index = c("SI","BGI","BI"))
+EX.Obj.Data<-fieldInfo_extra(mosaic = EX.Obj.I[[c("SI","BGI","BI")]], 
+                             fieldShape = EX.Obj.D)
+plot(EX.Obj.Data)
 
 # Data visualization: 
-Data.Obj1<-melt(Data.Obj[,c("SI","BGI","BI","VARI","area","x.dist","y.dist")])
+library(reshape2)
+Data.Obj1<-melt(EX.Obj.Data,measure.vars = c("area","perimeter","width","SI_mean","BGI_mean","BI_mean"))
 
+library(ggplot2)
 ggplot(Data.Obj1, aes(x=value, fill=variable)) +
   geom_histogram(aes(y=..density..), colour="black")+
   geom_density(alpha=.2)+
